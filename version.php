11d77cb267956c012b0a4146921d6919163ca8aa
barrasroger <roger.barras@htwchur.ch>
2017-06-30 18:02:02 +0200

    version increment

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017062610; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017062611; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016120500; // Requires this Moodle version
 $plugin->component = 'enrol_evento'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_BETA;
