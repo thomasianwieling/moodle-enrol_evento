01c938f83b2502d0c0ea5b813ab154aba2cc8d22
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2018-10-15 11:06:30 +0200

    Update test_test.php

--- tests/test_test.php
+++ tests/test_test.php
@@ -1,4 +1,36 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for activities completed by classification.
+ *
+ * @package   enrol evento
+ * @copyright 2018 HTW Chur Thomas Wieling
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Unit tests for activities completed by classification.
+ *
+ * @package   core_analytics
+ * @copyright 2017 David Monllaó {@link http://www.davidmonllao.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
  /** This class contains the test cases for the functions in editlib.php. */
  class mod_myplugin_sample_testcase extends advanced_testcase {
