333d901e377ebd4afaca6319ae1d3abf5e9f0364
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2018-10-15 11:00:06 +0200

    Moodle Example Test update

--- tests/test_test.php
+++ tests/test_test.php
@@ -1,9 +1,22 @@
 <?php
+ /**
+  * Unit tests for (some of) mod/quiz/editlib.php.
+  *
+  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+  * @package question
+  */
 
-class mod_myplugin_sample_testcase extends advanced_testcase {
-     public function test_adding() {
-         $this->assertEquals(2, 1+2);
-     }
+ if (!defined('MOODLE_INTERNAL')) {
+     die('Direct access to this script is forbidden.'); //  It must be included from a Moodle page
  }
 
- 
+ // Make sure the code being tested is accessible.
+ require_once($CFG->dirroot . '/mod/quiz/editlib.php'); // Include the code to test
+
+ /** This class contains the test cases for the functions in editlib.php. */
+ class mod_myplugin_sample_testcase extends advanced_testcase {
+      public function test_adding() {
+          $this->assertEquals(2, 1+2);
+      }
+  }
+ ?>
