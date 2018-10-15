bf2bf562067e56aaa51b9d95f8bd919c751dcb91
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2018-10-15 11:02:33 +0200

    Test change

--- tests/test_test.php
+++ tests/test_test.php
@@ -1,17 +1,4 @@
 <?php
- /**
-  * Unit tests for (some of) mod/quiz/editlib.php.
-  *
-  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
-  * @package question
-  */
-
- if (!defined('MOODLE_INTERNAL')) {
-     die('Direct access to this script is forbidden.'); //  It must be included from a Moodle page
- }
-
- // Make sure the code being tested is accessible.
- require_once($CFG->dirroot . '/mod/quiz/editlib.php'); // Include the code to test
 
  /** This class contains the test cases for the functions in editlib.php. */
  class mod_myplugin_sample_testcase extends advanced_testcase {
