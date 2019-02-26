0ba7ed7a7e90692fa87b718ca17ab510cafe6b28
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-02-26 10:21:42 +0100

    Added copyright

--- interface.php
+++ interface.php
@@ -1,6 +1,29 @@
 <?php
-//require_once($CFG->dirroot . '/enrol/evento/tests/evento_service_generator.php');
-//require_once($CFG->dirroot . '/local/evento/classes/evento_service.php');
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Evento enrolment plugin main library file.
+ *
+ * @package    enrol_evento
+ * @copyright  2019 HTW Chur Thomas Wieling
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//  require_once($CFG->dirroot . '/enrol/evento/tests/evento_service_generator.php');
+//  require_once($CFG->dirroot . '/local/evento/classes/evento_service.php');
 interface interface_evento_service
 {
   /**
