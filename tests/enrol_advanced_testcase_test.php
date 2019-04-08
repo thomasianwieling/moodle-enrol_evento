6d03105cccce153f196df1f137970ef8c3f77e7e
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-04-08 10:35:31 +0200

    Add existing moodle user

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -97,10 +97,17 @@ class mod_evento_advanced_testcase extends advanced_testcase {
         $evento_person = $builder->add_person("Teacher", "Mister", "mister.teacher@stud.htwchur.ch",  118000, 30040, true, 118000, $evento_personen_anmeldung);
         $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118000, 0, 1, 0, "S-1-5-21-2460181394-1097805571-3701207438-51315", "MisTe");
 
+        /*create existing moodle user*/
+        $evento_personen_anmeldung = $builder->add_personen_anmeldung("2019-02-17T00:00:00.000+01:00", "hoferlis", "2018-06-05T08:58:20.723+02:00", "auto" , 415864, 20216, 25491, 118000, $evento_status);
+        $evento_person = $builder->add_person("Max", "Fritz", "max.fritz@htwchur.ch",  118000, 30040, true, 118000, $evento_personen_anmeldung);
+        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118000, 0, 1, 0, "S-1-5-21-2460181394-1097805571-3701207438-51315", "MisTe");
+
         $this->simulator = $builder->service;
         $this->locallib = new enrol_evento_user_sync_exposed($this->simulator);
         $this->resetAfterTest(true);
         $this->create_moodle_course();
+
+
 
         $user1 = $this->getDataGenerator()->create_user(array('email' => 'max.fritz@htwchur.ch', 'username' => '2460181394-1097805571-3701207438-51316@htwchur.ch', 'firstname' => 'Max', 'lastname' => 'Fritz', 'timecreated' => 1548078299, 'timemodified' => 1548078299));
         $result = $DB->get_records('user', array('lastname' => 'Fritz'));
