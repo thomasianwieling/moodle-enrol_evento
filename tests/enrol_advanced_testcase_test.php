29fc88e5a6921f00e5bfef0ba4866e80273bf308
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-04-09 09:54:25 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -100,7 +100,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
         /*create existing moodle user*/
         $evento_personen_anmeldung = $builder->add_personen_anmeldung("2019-02-17T00:00:00.000+01:00", "hoferlis", "2018-06-05T08:58:20.723+02:00", "auto" , 415864, 20216, 25491, 118200, $evento_status);
         $evento_person = $builder->add_person("Max", "Fritz", "max.fritz@htwchur.ch",  118200, 30040, true, 118200, $evento_personen_anmeldung);
-        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 1, 0, "S-1-5-21-2460181394-1097805571-3701207438-51316", "MisTe");
+        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 0, 1, "S-1-5-21-2460181394-1097805571-3701207438-51000", "MaFri");
 
         $this->simulator = $builder->service;
         $this->locallib = new enrol_evento_user_sync_exposed($this->simulator);
@@ -109,7 +109,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
 
 
 
-        $user1 = $this->getDataGenerator()->create_user(array('email' => 'max.fritz@htwchur.ch', 'username' => '2460181394-1097805571-3701207438-51316@htwchur.ch', 'firstname' => 'Max', 'lastname' => 'Fritz', 'timecreated' => 1548078299, 'timemodified' => 1548078299));
+        $user1 = $this->getDataGenerator()->create_user(array('email' => 'max.fritz@htwchur.ch', 'username' => '2460181394-1097805571-3701207438-51000@htwchur.ch', 'firstname' => 'Max', 'lastname' => 'Fritz', 'timecreated' => 1548078299, 'timemodified' => 1548078299));
         $result = $DB->get_records('user', array('lastname' => 'Fritz'));
 
         $item = new \stdClass();
@@ -117,7 +117,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
         $item->data = (string)118200;
         $item->dataformat = 0;
         $uiditem = $DB->insert_record('user_info_data', $item);
-        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 1, 0, "S-1-5-21-2460181394-1097805571-3701207438-51316", "MaFri");
+        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 1, 0, "S-1-5-21-2460181394-1097805571-3701207438-51000", "MaFri");
     }
 
     /*Enable plugin method*/
