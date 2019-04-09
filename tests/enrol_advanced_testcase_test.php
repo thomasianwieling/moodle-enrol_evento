d05d8575edf1db00fbd1b1196903cafeb3009581
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-04-09 13:15:51 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -100,7 +100,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
         /*create existing moodle user*/
         $evento_personen_anmeldung = $builder->add_personen_anmeldung("2019-02-17T00:00:00.000+01:00", "hoferlis", "2018-06-05T08:58:20.723+02:00", "auto" , 415864, 20216, 25491, 118200, $evento_status);
         $evento_person = $builder->add_person("Max", "Fritz", "max.fritz@htwchur.ch",  118200, 30040, true, 118200, $evento_personen_anmeldung);
-        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 0, 1, "S-1-5-21-2460181394-1097805571-3701207438-51000", "MaFri");
+//        $ad_account = $builder->add_ad_account(0, "2019-02-17T00:00:00.000+01:00", "2019-02-17T00:00:00.000+01:00", 0, 118200, 0, 0, 1, "S-1-5-21-2460181394-1097805571-3701207438-51000", "MaFri");
 
         $this->simulator = $builder->service;
         $this->locallib = new enrol_evento_user_sync_exposed($this->simulator);
