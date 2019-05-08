52df820c98ae2f9bd643e3d60bcf1a4c9d03f807
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-05-08 09:49:10 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -206,7 +206,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
 var_dump("getuserexistinguser");
          /*Get user by evento person ID for user ID*/
         $person = $this->locallib->get_user_exposed($eventopersonid, $isstudent = true, $username = null);
-        $recors = $DB->get_records("user_info_data", array $conditions=null, $sort='', $fields='*', $limitfrom=0, $limitnum=0)
+        $recors = $DB->get_records("user_info_data", array $conditions=null, $sort='', $fields='*', $limitfrom=0, $limitnum=0);
         var_dump($recors);
         //$this->assertEquals($user->email, $this->get_mail_from_person_id($eventopersonid));
 
