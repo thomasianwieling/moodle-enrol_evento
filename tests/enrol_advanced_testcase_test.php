3ced8aa8df30c99a865edf778f7e08cd7b936196
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2019-05-10 16:17:38 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -210,8 +210,8 @@ $table = 'user_info_data';
          /*Get user by evento person ID for user ID*/
         $person = $this->locallib->get_user_exposed($eventopersonid, $isstudent = true, $username = null);
         //$result = $DB->get_record('user_info_data', array('data' => '118200'));
-        $result = $DB->get_records('user', array('lastname' => 'Fritz'));
-        $result1 = $DB->get_records('user_info_data', array('data' => '118200'));
+        //$result = $DB->get_records('user', array('lastname' => 'Fritz'));
+        //$result1 = $DB->get_records('user_info_data', array('data' => '118200'));
         //var_dump($this->$result);
         var_dump($person);
         //$this->assertEquals($user->email, $this->get_mail_from_person_id($eventopersonid));
