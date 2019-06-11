af65e0233e70f0eedf80dfa436e82f8bbbf33ee7
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2019-06-11 16:11:11 +0200

    Update locallib.php

--- locallib.php
+++ locallib.php
@@ -512,6 +512,7 @@ class enrol_evento_user_sync
 
         $sqlparams = array('eventoidshortname' => ENROL_EVENTO_UIF_EVENTOID, 'eventoid' => (string)$eventoid);
         $userlist = $DB->get_records_sql($sql, $sqlparams);
+        var_dump($sqlparams);
         return $userlist;
 
 
