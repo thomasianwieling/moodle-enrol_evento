f5c07f5c9666a1ff1eb5c50e9fa55f2ccd53ae7d
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2019-07-05 10:42:09 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -265,7 +265,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
       $person = $this->locallib->get_user_exposed($eventoid);
       //$this->assertEquals($person->email, $this->get_mail_from_person_id($eventoid));
       //$this->expectException(Error::class);
-      $this->expectException('moodle_exception: local_evento/cannotfindaduser (No Active Directory account for 999999)');
+      $this->expectException('moodle_exception')
      // $this->expectOutputString('local_evento/cannotfindaduser (No Active Directory account for '.$eventoid);
   }
 
