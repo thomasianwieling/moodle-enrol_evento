767021f305022e06c6e242d432f7e674e5e9e378
EDU03\wielinthoma1 <thomas.wieling@htwchur.ch>
2019-07-05 15:51:37 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -264,7 +264,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
        $eventoid = 999999;
        /*Get user by evento person ID for user ID*/
       $person = $this->locallib->get_user_exposed($eventoid);
-
+      var_dump($person);
        $this->expectExceptionMessage("hallo",$person);
       //var_dump($person);
       //$this->expectException(Error::class);
