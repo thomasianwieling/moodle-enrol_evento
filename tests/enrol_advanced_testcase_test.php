316e7177ef6603040a9879ed67c21a4835fe5a1e
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-07-09 15:29:53 +0200

    var_dump user in locallib

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -295,9 +295,8 @@ class mod_evento_advanced_testcase extends advanced_testcase {
        global $DB;
        $username = "2460181394-1097805571-3701207438-51000@fh-htwchur.ch";
        /*get user by username*/
-//       $person = $this->locallib->get_user_by_username_exposed($username);
- $user = $DB->get_record('user', array('username' => $username));
-       var_dump($user);
+       $person = $this->locallib->get_user_by_username_exposed($username);
+
        /*username from method equals username*/
        //  $this->assertEquals($person->username, $username);
    }
