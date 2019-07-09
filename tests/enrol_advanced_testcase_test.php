a21e31dd33beacfec36c32d287b11b6cedd66c9c
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-07-09 14:40:29 +0200

    Update enrol_advanced_testcase_test.php

--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -291,6 +291,7 @@ class mod_evento_advanced_testcase extends advanced_testcase {
    */
    public function get_user_by_username() {
        /*set username*/
+       global $DB;
        $username = "2460181394-1097805571-3701207438-51000@fh-htwchur.ch";
        /*get user by username*/
 //       $person = $this->locallib->get_user_by_username_exposed($username);
