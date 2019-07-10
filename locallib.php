0b5e289b6511d922459002e10ea8533fae6f91ae
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-07-10 08:35:07 +0200

    Update locallib.php

--- locallib.php
+++ locallib.php
@@ -544,7 +544,6 @@ class enrol_evento_user_sync
     protected function get_user_by_username($username) {
         global $DB;
         $user = $DB->get_record('user', array('username' => $username));
-        var_dump($user);
         return ($user == false) ? null : $user;
     }
     /**
