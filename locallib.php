c3df41f794e4a5bc3abb339aa2a6be4b668d0d0d
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-07-10 08:30:05 +0200

    changed if to origin

--- locallib.php
+++ locallib.php
@@ -545,7 +545,7 @@ class enrol_evento_user_sync
         global $DB;
         $user = $DB->get_record('user', array('username' => $username));
         var_dump($user);
-        return (false) ? null : $user;
+        return ($user == false) ? null : $user;
     }
     /**
      * Sets or inserts the user defined field eventoid, if it exists.
--- tests/enrol_advanced_testcase_test.php
+++ tests/enrol_advanced_testcase_test.php
@@ -293,11 +293,10 @@ class mod_evento_advanced_testcase extends advanced_testcase {
        /*set username*/
        global $DB;
        $username = "2460181394-1097805571-3701207438-51000@fh-htwchur.ch";
+       $user = $DB->get_record('user', array('username' => $username));
        /*get user by username*/
-       $person = $this->locallib->get_user_by_username_exposed($username);
-       var_dump("User by username");
-       
-       var_dump($person);
+//       $person = $this->locallib->get_user_by_username_exposed($username);
+       var_dump($user);
 
        /*username from method equals username*/
        //  $this->assertEquals($person->username, $username);
